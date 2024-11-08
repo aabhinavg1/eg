c7df3a05a13674971d88d0cda56fb55ef55f25a4
aabhinavg1 <tiwariabhinavak@gmail.com>
2024-11-08 20:57:47 +0530

    added the function for the mul

--- calc.cpp
+++ calc.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 int add(int a , int b);
+int mul (int a , int b);
 int main(){
 int result = add(4,5);
-std::cout <<"The result is "<< result << std::endl;
+int result_mul = mul(4,5);
+std::cout <<"The result of add  is "<< result << std::endl;
 }
